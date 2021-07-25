fa19be433466b51f0f1c785c0f33c91f2a68c8df
Prcnsi <ji.dozin@gmail.com>
2021-07-25 23:04:36 +0900

    7/25

---
+++ 10951.py
@@ -0,0 +1,6 @@
+while True:
+    try:
+        N,X=map(int,input().split())
+        print(N+X)
+    except:
+        break
---
+++ 10952.py
@@ -0,0 +1,5 @@
+while True:
+    N,X=map(int,input().split())
+    if N==0 and X==0:
+        break
+    print(N+X)
